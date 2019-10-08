5f8ebca0acd75125b6a811092df9291009269597
Skeeve2 <skeevyskeeve@gmail.com>
2019-10-08 13:51:26 -0400

    Update editor.lua

--- editor.lua
+++ editor.lua
@@ -85,7 +85,8 @@ function setXML()
     <panel class="spacer" color=""/>
     <button class="dropdown" rectAlignment="MiddleLeft" image="d_forward" minWidth="16" minHeight="18" onClick="bde2a4/dropdown"/>
     <button class="select_b" image="" colors="|rgba(178,178,178,0.75)|#C8C8C8|rgba(0.78,0.78,0.78,0.5)" height="25" rectAlignment="UpperCenter" offsetXY="-4 5"/>
-    <InputField class="transform" image="" characterLimit="6" characterValidation="Decimal"/>
+    <InputField class="transform" image="" characterLimit="6" characterValidation="Decimal" onValueChanged="bde2a4/setTransform"/>
+    <Toggle class="toggle" onValueChanged="bde2a4/setAttribute"/>
   </Defaults>
 
   <panel height="900" width="400" id="AEDITV" color="rgba(194,194,194,0.8)" rectAlignment="MiddleRight"> \ <!-- WTF? -->
@@ -102,15 +103,17 @@ function setXML()
 </VerticalScrollView>
 </HorizontalLayout>
 <panel height="29%" color="" rectAlignment="LowerRight">
-  <HorizontalLayout spacing="5" Width="95%" height="12%" rectAlignment="UpperLeft"> <InputField id="name" image="" text="Cube"/><InputField id="guid" image="" text="bde2a4" readOnly="true"/></HorizontalLayout>
+  <HorizontalLayout spacing="5" Width="95%" height="12%" rectAlignment="UpperLeft"> <InputField id="name" image="" text="Cube" onEndEdit="bde2a4/setName"/><InputField id="guid" image="" text="bde2a4" readOnly="true"/></HorizontalLayout>
   <VerticalScrollView id="AEDITV" height="88%" width="100%" class="scrollView" color="" rectAlignment="LowerRight" verticalScrollbarVisibility="Permanent" movementType="Clamped" scrollSensitivity="30">
   <TableLayout id="Table2" cellSpacing="0" autoCalculateHeight="1" padding="5 5 5 5" cellBackgroundColor="">
     <Row preferredHeight="30"><cell><text>X</text><inputfield class="transform"  id="xt" text="0"/></cell><cell><text>Y</text><inputfield class="transform" id="yt" text="0"/></cell><cell><text>Z</text><inputfield class="transform" id="zt" text="0"/></cell></Row>
-    <Row preferredHeight="30"><Cell><Toggle id="locked">Locked</Toggle></Cell><Cell><Toggle id="stp">Snap to Points</Toggle></Cell><Cell><Toggle id="stg">Snap to Grid</Toggle></Cell></Row>
-    <Row preferredHeight="30"><Cell><Toggle id="ar">Auto-Raise</Toggle></Cell><Cell><Toggle id="sticky">Sticky</Toggle></Cell><Cell><Toggle id="hands">Hands</Toggle></Cell></Row>
-    <Row preferredHeight="30"><Cell><Toggle id="hfd">Hide face-down</Toggle></Cell><Cell><Toggle id="rfw">Reveal FOW</Toggle></Cell><Cell><Toggle id="ifw">Ignore FOW</Toggle></Cell></Row>
-    <Row preferredHeight="30"><Cell><Toggle id="tooltip">Tooltip</Toggle></Cell><Cell><Toggle id="gridproj">Grid-Projection</Toggle></Cell><Cell><Toggle id="persistant">Persistent</Toggle></Cell></Row>
-    <Row preferredHeight="30"><Cell><Toggle id="interactable">Interactable</Toggle></Cell><Cell><Toggle id="gravity">Gravity</Toggle></Cell></Row>
+    <Row preferredHeight="30"><cell><text>XR</text><inputfield class="transform"  id="xr" text="0"/></cell><cell><text>YR</text><inputfield class="transform" id="yr" text="0"/></cell><cell><text>ZR</text><inputfield class="transform" id="zr" text="0"/></cell></Row>
+    <Row preferredHeight="30"><cell><text>XS</text><inputfield class="transform"  id="xs" text="0"/></cell><cell><text>YS</text><inputfield class="transform" id="ys" text="0"/></cell><cell><text>ZS</text><inputfield class="transform" id="zs" text="0"/></cell></Row>
+    <Row preferredHeight="30"><Cell><Toggle id="locked" class="toggle">Locked</Toggle></Cell><Cell><Toggle id="stp" class="toggle">Snap to Points</Toggle></Cell><Cell><Toggle id="stg" class="toggle">Snap to Grid</Toggle></Cell></Row>
+    <Row preferredHeight="30"><Cell><Toggle id="ar" class="toggle">Auto-Raise</Toggle></Cell><Cell><Toggle id="sticky" class="toggle">Sticky</Toggle></Cell><Cell><Toggle id="hands" class="toggle">Hands</Toggle></Cell></Row>
+    <Row preferredHeight="30"><Cell><Toggle id="hfd" class="toggle">Hide face-down</Toggle></Cell><Cell><Toggle id="rfw" class="toggle">Reveal FOW</Toggle></Cell><Cell><Toggle id="ifw" class="toggle">Ignore FOW</Toggle></Cell></Row>
+    <Row preferredHeight="30"><Cell><Toggle id="tooltip" class="toggle">Tooltip</Toggle></Cell><Cell><Toggle id="gridproj" class="toggle">Grid-Projection</Toggle></Cell><Cell><Toggle id="persistant" class="toggle">Persistent</Toggle></Cell></Row>
+    <Row preferredHeight="30"><Cell><Toggle id="interactable" class="toggle">Interactable</Toggle></Cell><Cell><Toggle id="gravity" class="toggle">Gravity</Toggle></Cell></Row>
     </TableLayout>
   </VerticalScrollView>
 </panel>
@@ -129,17 +132,21 @@ function setXML()
     if x.getName() ~= "" then
       name = x.getName()
     else
-      name = x.tag
+      if x.AssetBundle then
+        name = "AssetBundle"
+      else
+        name = x.tag
+      end
     end
 
     if x.tag == "Bag" then
       if type(countbag(x)) == "number" then
-        added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><button class="dropdown" id="edr_'.. edr ..'" n="'.. countbag(x) ..'" /><panel class="icon" image="gameobject"/><Text class="scrolltext">'.. name ..'</Text></HorizontalLayout></Row>'
+        added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><button class="dropdown" id="edr_'.. edr ..'" n="'.. countbag(x) ..'" /><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
       else
-        added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><panel class="spacer" minWidth="16"/><panel class="icon" image="gameobject"/><Text class="scrolltext">'.. name ..'</Text></HorizontalLayout></Row>'
+        added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><panel class="spacer" minWidth="16"/><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
       end
     else
-      added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><panel class="spacer" minWidth="16"/><panel class="icon" image="gameobject"/><Text class="scrolltext">'.. name ..'</Text></HorizontalLayout></Row>'
+      added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><panel class="spacer" minWidth="16"/><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
     end
   end
   finished = UI.getXml() .. initial .. added .. final -- finish!
@@ -157,44 +164,119 @@ function getProperties(_, guid, id)
   --
   UI.setAttribute(id, "colors", "rgba(178,178,178,0.75)|#FFFFFF|#C8C8C8|rgba(0.78,0.78,0.78,0.2)" )
   obj.highlightOn("Green")
+  if obj == lastobj then
+    for _, player in ipairs(Player.getPlayers()) do
+    if player.host == true then
+      player.lookAt({
+        position = obj.getPosition(),
+        pitch    = 25,
+        yaw      = 180,
+        distance = 20
+      })
+    end
 
-  local name = obj.getName()
-  local parameters = checkAttributes(obj)
-
-  if obj.getName() == "" then name = obj.tag end
-  UI.setAttribute("name", "text", name)
-  UI.setAttribute("xt", "text", parameters.transform.x)
-  UI.setAttribute("yt", "text", parameters.transform.y)
-  UI.setAttribute("zt", "text", parameters.transform.z)
-  UI.setAttribute("locked", "isOn", parameters.locked)
-  UI.setAttribute("stp", "isOn", parameters.STP)
-  UI.setAttribute("stg", "isOn", parameters.STG)
-  UI.setAttribute("ar", "isOn", parameters.raise)
-  UI.setAttribute("sticky", "isOn", parameters.sticky)
-  UI.setAttribute("hands", "isOn", parameters.hands)
-  UI.setAttribute("hfd", "isOn", parameters.hfd)
-  UI.setAttribute("rfw", "isOn", parameters.rfow)
-  UI.setAttribute("ifw", "isOn", parameters.ifow)
-  UI.setAttribute("tooltip", "isOn", parameters.tooltip)
-  UI.setAttribute("gridproj", "isOn", parameters.grid_proj)
-  UI.setAttribute("interactable", "isOn", parameters.interactable)
-  UI.setAttribute("gravity", "isOn", parameters.gravity)
-
-  lastui = id
-  lastobj = obj
+  end
+      return nil
+end
+    lastui = id
+    lastobj = obj
+  CV = false
+  Wait.frames(checkValues, 6) -- Prevents recursion. Courtines would be better but eff that lmao
+  Wait.frames(function() CV = true end, 5)
 end
 
 function selectNone()
   if lastobj then
     lastobj.highlightOff()
     UI.setAttribute(lastui, "colors", "|rgba(178,178,178,0.75)|#C8C8C8|rgba(0.78,0.78,0.78,0.5)")
+    CV = false
   else
     -- nothing
   end
 end
 
+function setAttribute(_, bool, id) -- Parameters make no sense.
+  if bool == "True" then bool = true else bool = false end
+  if id == "locked" then
+    lastobj.setLock(bool)
+  elseif id == "stp" then
+    lastobj.use_snap_points = bool
+  elseif id == "stg" then
+    lastobj.use_grid = bool
+  elseif id == "ar" then
+    lastobj.auto_raise = bool
+  elseif id == "sticky" then
+    lastobj.sticky = bool
+  elseif id == "hands" then
+    lastobj.use_hands = bool
+  elseif id == "hfd" then
+    lastobj.hide_when_face_down = bool
+  elseif id == "rfw" then
+    lastobj.setFogOfWarReveal({reveal = bool})
+  elseif id == "ifw" then
+    lastobj.ignore_fog_of_war = bool
+  elseif id == "tooltip" then
+    lastobj.tooltip = bool
+  elseif id == "gridproj" then
+    lastobj.grid_projection = bool
+  elseif id == "interactable" then
+    lastobj.interactable = bool
+  elseif id == "gravity" then
+    lastobj.use_gravity = bool
+  end
+end
 
+function setName(_,text)
+  lastobj.setName(text)
+  local p = string.gsub(lastui, "_btn", "")
+  UI.setAttribute(p .. "n", "text", text)
+end
 
+function checkValues()
+    local obj = lastobj
+    local name = obj.getName()
+    local parameters = checkAttributes(obj)
+
+    if obj.getName() == "" then name = obj.tag end
+    UI.setAttribute("name", "text", name)
+    UI.setAttribute("xt", "text", parameters.transform.x)
+    UI.setAttribute("yt", "text", parameters.transform.y)
+    UI.setAttribute("zt", "text", parameters.transform.z)
+    UI.setAttribute("xr", "text", parameters.rotation.x)
+    UI.setAttribute("yr", "text", parameters.rotation.y)
+    UI.setAttribute("zr", "text", parameters.rotation.z)
+    UI.setAttribute("xs", "text", parameters.scale.x)
+    UI.setAttribute("ys", "text", parameters.scale.y)
+    UI.setAttribute("zs", "text", parameters.scale.z)
+    UI.setAttribute("locked", "isOn", parameters.locked)
+    UI.setAttribute("stp", "isOn", parameters.STP)
+    UI.setAttribute("stg", "isOn", parameters.STG)
+    UI.setAttribute("ar", "isOn", parameters.raise)
+    UI.setAttribute("sticky", "isOn", parameters.sticky)
+    UI.setAttribute("hands", "isOn", parameters.hands)
+    UI.setAttribute("hfd", "isOn", parameters.hfd)
+    UI.setAttribute("rfw", "isOn", parameters.rfow)
+    UI.setAttribute("ifw", "isOn", parameters.ifow)
+    UI.setAttribute("tooltip", "isOn", parameters.tooltip)
+    UI.setAttribute("gridproj", "isOn", parameters.grid_proj)
+    UI.setAttribute("interactable", "isOn", parameters.interactable)
+    UI.setAttribute("gravity", "isOn", parameters.gravity)
+
+  if CV == true then
+    Wait.frames(checkValues, 5)
+  end
+end
+
+function setTransform(_,t,id)
+  local cpos = lastobj.getPosition()
+  if id == "xt" then
+    lastobj.setPosition({t, cpos.y, cpos.z})
+  elseif id == "yt" then
+    lastobj.setPosition({cpos.x, t, cpos.z})
+  elseif id == "zt" then
+    lastobj.setPosition({cpos.x, cpos.y, t})
+  end
+end
 ---------------
 -- Utility
 ---------------
@@ -211,22 +293,24 @@ function countbag(bag) -- Returns # of items in a bag and false if none
 end
 
 function checkAttributes(obj) -- Returns a table of the objects attributes
-local t = {
-  transform = obj.getPosition(),
-  guid = obj.getGUID(),
-  locked = obj.getLock(),
-  STP = obj.use_snap_points,
-  STG = obj.use_grid,
-  raise = obj.auto_raise,
-  sticky = obj.sticky,
-  hands = obj.use_hands,
-  hfd = obj.hide_when_face_down,
-  rfow = obj.getFogOfWarReveal().reveal,
-  ifow = obj.ignore_fog_of_war,
-  tooltip = obj.tooltip,
-  grid_proj = obj.grid_projection,
-  interactable = obj.interactable,
-  gravity = obj.use_gravity
-}
-return t
+  local t = {
+    transform = obj.getPosition(),
+    rotation = obj.getRotation(),
+    scale = obj.getScale(),
+    guid = obj.getGUID(),
+    locked = obj.getLock(),
+    STP = obj.use_snap_points,
+    STG = obj.use_grid,
+    raise = obj.auto_raise,
+    sticky = obj.sticky,
+    hands = obj.use_hands,
+    hfd = obj.hide_when_face_down,
+    rfow = obj.getFogOfWarReveal().reveal,
+    ifow = obj.ignore_fog_of_war,
+    tooltip = obj.tooltip,
+    grid_proj = obj.grid_projection,
+    interactable = obj.interactable,
+    gravity = obj.use_gravity
+  }
+  return t
 end
