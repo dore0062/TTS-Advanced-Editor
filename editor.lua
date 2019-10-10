d726ac1fa69b203642e6794752cd5944e37a4778
Skeeve2 <skeevyskeeve@gmail.com>
2019-10-10 18:49:11 -0400

    Update editor.lua

--- editor.lua
+++ editor.lua
@@ -3,6 +3,7 @@ function onload()
   setXML()
 end
 
+
 ---------------
 -- Menu interaction
 ---------------
@@ -14,7 +15,6 @@ function dropdown(_, _, id)
   local p = string.gsub(id, "r", "")
 
   if n == nil then n = 0 end
-
   local md = n * 25
 
   UI.setAttribute(p, "active", "true")
@@ -133,184 +133,254 @@ function setXML()
       name = x.getName()
     else
       if x.AssetBundle then
-        name = "AssetBundle"
+        local t = x.getChildren()
+        name = tostring(t[1])
       else
         name = x.tag
       end
     end
 
-    if x.tag == "Bag" then
-      if type(countbag(x)) == "number" then
-        added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><button class="dropdown" id="edr_'.. edr ..'" n="'.. countbag(x) ..'" /><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
+    if x.AssetBundle then
+      local t = x.getChildren() -- Top level only
+      local b = extractChild(x, {tostring(t[1])}).getChildren()
+      if b[1] then
+        local toadd, numadd = recursiveExtraction(edr, t, x)
+        local id = '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><button class="dropdown" id="edr_'.. edr ..'" n="'.. numadd ..'" /><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
+        added = added .. id .. toadd
       else
         added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><panel class="spacer" minWidth="16"/><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
       end
-    else
-      added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><panel class="spacer" minWidth="16"/><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
+      else if x.tag == "Bag" then
+        if type(countbag(x)) == "number" then
+          added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><button class="dropdown" id="edr_'.. edr ..'" n="'.. countbag(x) ..'" /><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
+        else
+          added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><panel class="spacer" minWidth="16"/><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
+        end
+      else
+        added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edr .. '_btn" onClick="bde2a4/getProperties('.. x.getGUID() ..')"/><HorizontalLayout class="width"><panel class="spacer" minWidth="16"/><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edr ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
+      end
     end
   end
+
   finished = UI.getXml() .. initial .. added .. final -- finish!
   Global.UI.setXml(finished)
 end
 
-function getProperties(_, guid, id)
-  local obj = getObjectFromGUID(guid)
+function recursiveExtraction(edr, children_table, obj, level) -- Extracts all components of an assetbundle
+  if not level then level = 1 end
 
-  -- Reset last selected
-  if lastobj then
-    lastobj.highlightOff()
-    UI.setAttribute(lastui, "colors", "|rgba(178,178,178,0.75)|#C8C8C8|rgba(0.78,0.78,0.78,0.5)" )
+  local spacer = 16 * level -- Controls how far the tab is
+  local added = ""
+  local start = '<Row preferredHeight="60" id="ed_'.. edr ..'" active="false"><TableLayout cellSpacing = "-6" >'
+  local finish = '</TableLayout></Row>'
+  local edm = 0
+  for l, b in pairs(children_table) do
+    edm = edm + 1
+    local edt = edr .. "_" .. edm
+
+    if b.getChildren() then
+      local toadd, numadd = recursiveExtraction(edt, b.getChildren(), obj, level + 1)
+      id = '<Row preferredHeight="30"><button class ="select_b" id = "'.. edt ..'_btn" onClick="bde2a4/nil"/><HorizontalLayout class="width"><panel class="spacer" minWidth="'.. spacer ..'"/><button class="dropdown" id="edr_'.. edt ..'" n="'.. numadd ..'" /><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edt ..'n" text = "'.. tostring(b) ..'"/></HorizontalLayout></Row>'
+      added = added .. id .. toadd
+    else
+      added = added .. '<Row preferredHeight="30"><button class ="select_b" id = "'.. edt ..'_btn" onClick="bde2a4/nil"/><HorizontalLayout class="width"><panel class="spacer" minWidth="'.. spacer ..'"/><panel class="icon" image="gameobject"/><Text class="scrolltext" id="'.. edt ..'n" text = "'.. name ..'"/></HorizontalLayout></Row>'
+    end
   end
-  --
-  UI.setAttribute(id, "colors", "rgba(178,178,178,0.75)|#FFFFFF|#C8C8C8|rgba(0.78,0.78,0.78,0.2)" )
-  obj.highlightOn("Green")
-  if obj == lastobj then
-    for _, player in ipairs(Player.getPlayers()) do
+
+if edm == 0 then
+  return "", 0
+else
+
+  return start .. added .. finish, edm
+end
+end
+
+
+function getProperties(_, guid, id)
+local obj = getObjectFromGUID(guid)
+
+-- Reset last selected
+if lastobj then
+  lastobj.highlightOff()
+  UI.setAttribute(lastui, "colors", "|rgba(178,178,178,0.75)|#C8C8C8|rgba(0.78,0.78,0.78,0.5)" )
+end
+--
+UI.setAttribute(id, "colors", "rgba(178,178,178,0.75)|#FFFFFF|#C8C8C8|rgba(0.78,0.78,0.78,0.2)" )
+obj.highlightOn("Green")
+if obj == lastobj then
+  for _, player in ipairs(Player.getPlayers()) do
     if player.host == true then
       player.lookAt({
         position = obj.getPosition(),
-        pitch    = 25,
-        yaw      = 180,
+        pitch = 25,
+        yaw = 180,
         distance = 20
       })
     end
 
   end
-      return nil
+  return nil
 end
-    lastui = id
-    lastobj = obj
-  CV = false
-  Wait.frames(checkValues, 6) -- Prevents recursion. Courtines would be better but eff that lmao
-  Wait.frames(function() CV = true end, 5)
+lastui = id
+lastobj = obj
+CV = false
+
+local name = obj.getName()
+if obj.getName() == "" then name = "" end
+UI.setAttribute("name", "text", name)
+
+Wait.frames(checkValues, 6) -- Prevents recursion. Courtines would be better but eff that lmao
+Wait.frames(function() CV = true end, 5)
 end
 
 function selectNone()
-  if lastobj then
-    lastobj.highlightOff()
-    UI.setAttribute(lastui, "colors", "|rgba(178,178,178,0.75)|#C8C8C8|rgba(0.78,0.78,0.78,0.5)")
-    CV = false
-  else
-    -- nothing
-  end
+if lastobj then
+  lastobj.highlightOff()
+  UI.setAttribute(lastui, "colors", "|rgba(178,178,178,0.75)|#C8C8C8|rgba(0.78,0.78,0.78,0.5)")
+  CV = false
+else
+  -- nothing
+end
 end
 
 function setAttribute(_, bool, id) -- Parameters make no sense.
-  if bool == "True" then bool = true else bool = false end
-  if id == "locked" then
-    lastobj.setLock(bool)
-  elseif id == "stp" then
-    lastobj.use_snap_points = bool
-  elseif id == "stg" then
-    lastobj.use_grid = bool
-  elseif id == "ar" then
-    lastobj.auto_raise = bool
-  elseif id == "sticky" then
-    lastobj.sticky = bool
-  elseif id == "hands" then
-    lastobj.use_hands = bool
-  elseif id == "hfd" then
-    lastobj.hide_when_face_down = bool
-  elseif id == "rfw" then
-    lastobj.setFogOfWarReveal({reveal = bool})
-  elseif id == "ifw" then
-    lastobj.ignore_fog_of_war = bool
-  elseif id == "tooltip" then
-    lastobj.tooltip = bool
-  elseif id == "gridproj" then
-    lastobj.grid_projection = bool
-  elseif id == "interactable" then
-    lastobj.interactable = bool
-  elseif id == "gravity" then
-    lastobj.use_gravity = bool
-  end
+if bool == "True" then bool = true else bool = false end
+if id == "locked" then
+  lastobj.setLock(bool)
+elseif id == "stp" then
+  lastobj.use_snap_points = bool
+elseif id == "stg" then
+  lastobj.use_grid = bool
+elseif id == "ar" then
+  lastobj.auto_raise = bool
+elseif id == "sticky" then
+  lastobj.sticky = bool
+elseif id == "hands" then
+  lastobj.use_hands = bool
+elseif id == "hfd" then
+  lastobj.hide_when_face_down = bool
+elseif id == "rfw" then
+  lastobj.setFogOfWarReveal({reveal = bool})
+elseif id == "ifw" then
+  lastobj.ignore_fog_of_war = bool
+elseif id == "tooltip" then
+  lastobj.tooltip = bool
+elseif id == "gridproj" then
+  lastobj.grid_projection = bool
+elseif id == "interactable" then
+  lastobj.interactable = bool
+elseif id == "gravity" then
+  lastobj.use_gravity = bool
+end
 end
 
-function setName(_,text)
-  lastobj.setName(text)
-  local p = string.gsub(lastui, "_btn", "")
-  UI.setAttribute(p .. "n", "text", text)
+function setName(_, text)
+lastobj.setName(text)
+local p = string.gsub(lastui, "_btn", "")
+UI.setAttribute(p .. "n", "text", text)
 end
 
 function checkValues()
-    local obj = lastobj
-    local name = obj.getName()
-    local parameters = checkAttributes(obj)
+local obj = lastobj
+local parameters = checkAttributes(obj)
 
-    if obj.getName() == "" then name = obj.tag end
-    UI.setAttribute("name", "text", name)
-    UI.setAttribute("xt", "text", parameters.transform.x)
-    UI.setAttribute("yt", "text", parameters.transform.y)
-    UI.setAttribute("zt", "text", parameters.transform.z)
-    UI.setAttribute("xr", "text", parameters.rotation.x)
-    UI.setAttribute("yr", "text", parameters.rotation.y)
-    UI.setAttribute("zr", "text", parameters.rotation.z)
-    UI.setAttribute("xs", "text", parameters.scale.x)
-    UI.setAttribute("ys", "text", parameters.scale.y)
-    UI.setAttribute("zs", "text", parameters.scale.z)
-    UI.setAttribute("locked", "isOn", parameters.locked)
-    UI.setAttribute("stp", "isOn", parameters.STP)
-    UI.setAttribute("stg", "isOn", parameters.STG)
-    UI.setAttribute("ar", "isOn", parameters.raise)
-    UI.setAttribute("sticky", "isOn", parameters.sticky)
-    UI.setAttribute("hands", "isOn", parameters.hands)
-    UI.setAttribute("hfd", "isOn", parameters.hfd)
-    UI.setAttribute("rfw", "isOn", parameters.rfow)
-    UI.setAttribute("ifw", "isOn", parameters.ifow)
-    UI.setAttribute("tooltip", "isOn", parameters.tooltip)
-    UI.setAttribute("gridproj", "isOn", parameters.grid_proj)
-    UI.setAttribute("interactable", "isOn", parameters.interactable)
-    UI.setAttribute("gravity", "isOn", parameters.gravity)
-
-  if CV == true then
-    Wait.frames(checkValues, 5)
+if obj.getName() == "" then
+  if obj.AssetBundle then
+    local t = obj.getChildren()
+    name = tostring(t[1])
+  else
+    name = obj.tag
   end
+  setName(_, name)
 end
 
-function setTransform(_,t,id)
-  local cpos = lastobj.getPosition()
-  if id == "xt" then
-    lastobj.setPosition({t, cpos.y, cpos.z})
-  elseif id == "yt" then
-    lastobj.setPosition({cpos.x, t, cpos.z})
-  elseif id == "zt" then
-    lastobj.setPosition({cpos.x, cpos.y, t})
-  end
+UI.setAttribute("xt", "text", parameters.transform.x)
+UI.setAttribute("yt", "text", parameters.transform.y)
+UI.setAttribute("zt", "text", parameters.transform.z)
+UI.setAttribute("xr", "text", parameters.rotation.x)
+UI.setAttribute("yr", "text", parameters.rotation.y)
+UI.setAttribute("zr", "text", parameters.rotation.z)
+UI.setAttribute("xs", "text", parameters.scale.x)
+UI.setAttribute("ys", "text", parameters.scale.y)
+UI.setAttribute("zs", "text", parameters.scale.z)
+UI.setAttribute("locked", "isOn", parameters.locked)
+UI.setAttribute("stp", "isOn", parameters.STP)
+UI.setAttribute("stg", "isOn", parameters.STG)
+UI.setAttribute("ar", "isOn", parameters.raise)
+UI.setAttribute("sticky", "isOn", parameters.sticky)
+UI.setAttribute("hands", "isOn", parameters.hands)
+UI.setAttribute("hfd", "isOn", parameters.hfd)
+UI.setAttribute("rfw", "isOn", parameters.rfow)
+UI.setAttribute("ifw", "isOn", parameters.ifow)
+UI.setAttribute("tooltip", "isOn", parameters.tooltip)
+UI.setAttribute("gridproj", "isOn", parameters.grid_proj)
+UI.setAttribute("interactable", "isOn", parameters.interactable)
+UI.setAttribute("gravity", "isOn", parameters.gravity)
+
+if CV == true then
+  Wait.frames(checkValues, 5)
+end
+end
+
+function setTransform(_, t, id)
+local cpos = lastobj.getPosition()
+if id == "xt" then
+  lastobj.setPosition({t, cpos.y, cpos.z})
+elseif id == "yt" then
+  lastobj.setPosition({cpos.x, t, cpos.z})
+elseif id == "zt" then
+  lastobj.setPosition({cpos.x, cpos.y, t})
+end
 end
 ---------------
 -- Utility
 ---------------
 function countbag(bag) -- Returns # of items in a bag and false if none
-  local i = 0
-  if bag.getObjects()[1] then
-    for x, b in pairs(bag.getObjects()) do
-      i = i + 1
-    end
-  else
-    return false
+local i = 0
+if bag.getObjects()[1] then
+  for x, b in pairs(bag.getObjects()) do
+    i = i + 1
   end
-  return i
+else
+  return false
+end
+return i
 end
 
 function checkAttributes(obj) -- Returns a table of the objects attributes
-  local t = {
-    transform = obj.getPosition(),
-    rotation = obj.getRotation(),
-    scale = obj.getScale(),
-    guid = obj.getGUID(),
-    locked = obj.getLock(),
-    STP = obj.use_snap_points,
-    STG = obj.use_grid,
-    raise = obj.auto_raise,
-    sticky = obj.sticky,
-    hands = obj.use_hands,
-    hfd = obj.hide_when_face_down,
-    rfow = obj.getFogOfWarReveal().reveal,
-    ifow = obj.ignore_fog_of_war,
-    tooltip = obj.tooltip,
-    grid_proj = obj.grid_projection,
-    interactable = obj.interactable,
-    gravity = obj.use_gravity
-  }
-  return t
+local t = {
+  transform = obj.getPosition(),
+  rotation = obj.getRotation(),
+  scale = obj.getScale(),
+  guid = obj.getGUID(),
+  locked = obj.getLock(),
+  STP = obj.use_snap_points,
+  STG = obj.use_grid,
+  raise = obj.auto_raise,
+  sticky = obj.sticky,
+  hands = obj.use_hands,
+  hfd = obj.hide_when_face_down,
+  rfow = obj.getFogOfWarReveal().reveal,
+  ifow = obj.ignore_fog_of_war,
+  tooltip = obj.tooltip,
+  grid_proj = obj.grid_projection,
+  interactable = obj.interactable,
+  gravity = obj.use_gravity
+}
+return t
+end
+
+function extractChild(element, children) -- Credit to dzikakulka. Extracts children/components easily
+assert(type(children) == "table", "children must be in table")
+logStyle("error", "Red")
+for _, name in ipairs(children) do
+  assert(type(name) == "string", "children names must be a string")
+  if element.getChild(name) == nil then
+    log(element.getChildren(), "children available:", "error")
+    log(children, "children selected:", "error")
+    error("child does not exist: " .. name)
+  end
+  element = element.getChild(name)
+end
+return element
 end
